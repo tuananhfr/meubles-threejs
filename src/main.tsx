dac202ebe2f3284aa865ec09089b38a15e1e41fd
PHAM Tuan Anh <phamtuananh3031998@gmail.com>
2025-07-01 23:48:18 +0200

    add cdn bootstrap js

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import ConfigProvider from "./components/context/ConfigProvider.tsx";
 
